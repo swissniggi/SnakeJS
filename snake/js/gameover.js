b20840094f7e7f9c439f93a453536de5d955362f
Nicolas Burgunder <nicolas.burgunder@gmail.com>
2018-11-05 13:33:22 +0100

    Update gameover.js

--- snake/js/gameover.js
+++ snake/js/gameover.js
@@ -53,15 +53,15 @@ kijs.Class.define('snake.GameOver', {
         
         prepareMsg: function() {
             // Game-Over-Nachricht definieren
-            if (this.maxScore == this.highScore[0] && this.highScore[1] === '' && this.maxScore <= this.highScore[3] && this.maxScore <= this.highScore[6]) {
+            if (this.highScore[9] === 'W') {
                 this.msg = '<p style="font-size:150%">Game Over!</p>' + this.msg
                          + '<p class="highscore_new"><u>Neuer Wochen-Highscore!</u></p>';
                 this.showGameOver(true);
-            } else if (this.maxScore == this.highScore[0] && this.maxScore == this.highScore[3] && this.highScore[4] === '' && this.maxScore <= this.highScore[6]) {
+            } else if (this.highScore[9] === 'M') {
                 this.msg = '<p style="font-size:150%">Game Over!</p>' + this.msg
                          + '<p class="highscore_new"><u>Neuer Monats-Highscore!</u></p>';
                 this.showGameOver(true);
-            } else if (this.maxScore == this.highScore[0] && this.maxScore == this.highScore[3] && this.maxScore == this.highScore[6] && this.highScore[7] === '') {
+            } else if (this.highScore[9] === 'Y') {
                 this.msg = '<p style="font-size:150%">Game Over!</p>' + this.msg
                          + '<p class="highscore_new"><u>Neuer Jahres-Highscore!</u></p>';
                 this.showGameOver(true);
